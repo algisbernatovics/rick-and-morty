d746eabd956c39a0e1688468c1c61be799c10350
algisbernatovics <algis.bernatovics@gmail.com>
2023-10-22 19:40:22 +0300

    Implementing cache

--- app/Core/Functions.php
+++ app/Core/Functions.php
@@ -4,19 +4,8 @@ namespace App\Core;
 
 class Functions
 {
-    public static function replaceSlash(string $uri)
-    {
-        return str_replace('/', '', $uri);
-    }
-
     public static function cutUri(string $episodeUri)
     {
         return substr($episodeUri, 32);
     }
-
-    public static function cleanDateStr(string $expires_at)
-    {
-        return preg_replace('/[^0-9]/', " ", $expires_at, -1);
-    }
-
 }
