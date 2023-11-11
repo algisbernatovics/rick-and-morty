86bca860af6dca74b92bf043986a9e684c7bf1f0
algisbernatovics <algis.bernatovics@gmail.com>
2023-11-11 15:36:51 +0200

    fix

--- public_html/scripts/scripts.js
+++ public_html/scripts/scripts.js
@@ -2,9 +2,8 @@ const searchButton = document.getElementById("search-button");
 const filter = document.getElementById("filter");
 
 searchButton.addEventListener("click", function () {
-    if (filter.style.display === "none" || filter.style.display === "") {
-        filter.style.display = "block";
-    } else {
-        filter.style.display = "none";
-    }
+
+    filter.style.display = (filter.style.display === 'none' || filter.style.display === '') ? 'block' : 'none';
+
+    searchButton.classList.toggle("active");
 });
