0a0a74e802a14865e5ee661680554c3522a61d81
Algis @ home <algis.bernatovics@gmail.com>
2024-04-13 21:26:29 +0300

    adaptive to os dark/light mode toggle

--- public_html/scripts/dark-mode-toggle.js
+++ public_html/scripts/dark-mode-toggle.js
@@ -1,42 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-    console.log('Local Storage Theme:', localStorage.theme);
-
-    const switchInput = document.getElementById('darkLightSwitch');
+    console.log('Dark Theme', prefersDark);
 
     // Function to update the theme on page load
     function updateThemeOnLoad() {
-        const currentTheme = localStorage.theme;
-        if (currentTheme === 'dark') {
+        console.log('updateThemeOnLoad called');
+        if (prefersDark === true) {
             document.documentElement.classList.add('dark');
-            switchInput.checked = true;
         } else {
             document.documentElement.classList.remove('dark');
-            switchInput.checked = false;
         }
     }
 
-    // Function to toggle dark mode
-    window.toggleDarkMode = function () {
-        if (localStorage.theme === 'dark') {
-            localStorage.theme = 'light';
-        } else {
-            localStorage.theme = 'dark';
-        }
-
-        document.documentElement.classList.toggle('dark');
-        switchInput.checked = document.documentElement.classList.contains('dark');
-        console.log('Local Storage Theme set to:', localStorage.theme);
-    };
-
-    // Function to toggle the switch
-    function toggleSwitch() {
-        window.toggleDarkMode(); // Toggle the theme when the switch is clicked
-    }
-
-    // Attach the toggleSwitch function to the switch input
-    switchInput.addEventListener('click', toggleSwitch);
-
-    // Set the initial theme on page load
     updateThemeOnLoad();
 });
